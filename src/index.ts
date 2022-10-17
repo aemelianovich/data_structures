e75dc5fda3b9e00c74e995c3a31315ac540759d6
aemelianovich <aemelianovich@yandex.ru>
2022-10-17 00:36:20 -0500

    Queue

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { default as Structure } from './data_structures/structure';
 export { default as Stack } from './data_structures/stack';
 export { default as DoublyLinkedList } from './data_structures/doubly_linked_list';
+export { default as Queue } from './data_structures/queue';
