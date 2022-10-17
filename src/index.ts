12014b8442c46c1bf4716a139501024bcdf107c2
aemelianovich <aemelianovich@yandex.ru>
2022-10-17 00:32:07 -0500

    DoublyLinkedList

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 export { default as Structure } from './data_structures/structure';
 export { default as Stack } from './data_structures/stack';
 export { default as DoublyLinkedList } from './data_structures/doubly_linked_list';
-export { default as Queue } from './data_structures/queue';
